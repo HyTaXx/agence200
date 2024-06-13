c3673453f38de2ef1af7795893d33c3d92c877e1
HyTaXx <nobrearmand@gmail.com>
2024-06-13 10:03:56 +0200

    fix

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,21 +1,34 @@
 export const Hero = () => {
     return (
-      <section className="flex flex-row text-black md:px-20 my-20">
-        <div className="flex flex-col justify-around w-1/2 gap-8">
-          <h3 className="text-xl">Commencez Dès Aujourd'hui</h3>
-          <h1 className="text-6xl leading-normal">Apprenez A<br></br> 
-            <div className="relative">
-              <img src="./images/stain.png" alt="stain" className="absolute h-full"/>
-              <span className="relative z-10">Protéger Votre</span>
+        <section className="flex flex-row text-black md:px-20 my-20">
+            <div className="flex flex-col justify-around w-1/2 gap-8">
+                <h3 className="text-xl">Commencez dès aujourd'hui</h3>
+                <h1 className="text-6xl leading-normal">
+                    Apprenez a<br></br>
+                    <div className="relative">
+                        <img
+                            src="./images/stain.png"
+                            alt="stain"
+                            className="absolute h-full"
+                        />
+                        <span className="relative z-10">protéger votre</span>
+                    </div>
+                    vie en ligne
+                </h1>
+                <p className="text-xl">
+                    L'agence 200 vous apprendre comment, VOUS,<br></br> jeunes
+                    entrepreneurs pouvez protéger vos données
+                </p>
+                <a
+                    href="#"
+                    className="bg-[#EB5E28] rounded-full py-3 w-[250px] text-white text-center"
+                >
+                    Nous contacter
+                </a>
             </div>
-            Vie En Ligne
-          </h1>
-          <p className="text-xl">L'agence 200 vous apprendre comment, VOUS,<br></br> Jeunes entrepreneurs pouvez protéger vos données</p>
-          <a href="#" className="bg-[#EB5E28] rounded-full py-3 w-[250px] text-white text-center">Nous contacter</a>
-        </div>
-        <div className="w-1/2">
-          <img src="images/Hero.svg" alt="Hero"/>
-        </div>
-      </section>
-    )
-  }
+            <div className="w-1/2">
+                <img src="images/Hero.svg" alt="Hero" />
+            </div>
+        </section>
+    );
+};
